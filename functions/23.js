ba70dc49276bc872fa4e3b4189ec972b51b2355a
David Taylor <david.taylor@necsws.com>
2025-10-03 11:02:16 +0100

    functions 23 solved

--- functions/23.js
+++ functions/23.js
@@ -6,7 +6,17 @@
  * Example: determineSeason(4) should return 'Spring'.
  */
 export function determineSeason(month) {
-  // TODO: ???
+  const seasons = {
+    'Winter' : [12, 1, 2],
+    'Spring' : [3, 4, 5],
+    'Summer' : [6, 7, 8],
+    'Autumn' : [9, 10, 11]
+  }
+    for(const season in seasons){
+      if(seasons[season].includes(month)){
+        return season
+      }
+    }
   // Winter: December (12), January (1), February (2)
   // Spring: March (3), April (4), May (5)
   // Summer: June (6), July (7), August (8)
