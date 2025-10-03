ea52114aa9bdf8d4f2fbf8a895078fd3d47fd560
David Taylor <david.taylor@necsws.com>
2025-10-03 10:57:33 +0100

    functions 22 solved

--- functions/22.js
+++ functions/22.js
@@ -4,12 +4,24 @@
  * @returns {string} The letter grade.
  *
  * Example: determineGrade(85) should return 'B'.
- */
+*/
+// 90-100: 'A'
+// 80-89: 'B'
+// 70-79: 'C'
+// 60-69: 'D'
+// Below 60: 'F'
 export function determineGrade(score) {
-  // TODO: ???
-  // 90-100: 'A'
-  // 80-89: 'B'
-  // 70-79: 'C'
-  // 60-69: 'D'
-  // Below 60: 'F'
+  const grades = {
+    'F' : [0, 59],
+    'D' : [60, 69],
+    'C' : [70, 79],
+    'B' : [80, 89],
+    'A' : [90, 100]
+  }
+  let gradeAchieved = ''
+  for(const grade in grades){
+    if(score >= grades[grade][0] && score <= grades[grade][1]){
+      return grade
+    }
+  }
 }
